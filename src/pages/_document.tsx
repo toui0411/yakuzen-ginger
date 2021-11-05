31122460e77ab20e321653d6101b08057e594812
toui0411 <riyu.93001@gmail.com>
2021-11-05 19:38:14 +0900

    update

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -18,7 +18,7 @@ class MyDocument extends Document {
 
   render(): JSX.Element {
     return (
-      <Html lang="en">
+      <Html lang="ja">
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin={"true"} />
